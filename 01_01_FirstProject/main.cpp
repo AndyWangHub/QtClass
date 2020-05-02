7d4389d2e83745bd4593db06d907e834526612ea
wangyihub <412461202@qq.com>
2020-05-02 13:17:06 +0800

    添加注释

--- 01_01_FirstProject/main.cpp
+++ 01_01_FirstProject/main.cpp
@@ -2,10 +2,17 @@
 
 #include <QApplication>
 
+//main程序入口
 int main(int argc, char *argv[])
 {
+    //a应用程序对象，在Qt中，应用程序对象 有且仅有一个
     QApplication a(argc, argv);
+    //窗口对象
     MyWidget w;
+    //窗口对象默认不会显示，必须要调用show方法显示窗口
     w.show();
+
+    //让应用程序对象进入消息循环
+    //当代码阻塞到这行
     return a.exec();
 }
